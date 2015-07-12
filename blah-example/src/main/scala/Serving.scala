f1ea2370dda3dae83c9592b53a2af153bef7b587
Dennis Dietrich <d.dietrich84@gmail.com>
2015-07-12 08:18:14 +0200

    ws

--- blah-example/src/main/scala/Serving.scala
+++ blah-example/src/main/scala/Serving.scala
@@ -1,9 +1,12 @@
 package blah.example
 
+import scala.concurrent.duration._
 import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.model.ws.TextMessage
+import akka.stream.scaladsl.{Sink, Source, Flow}
 import Directives._
 import spray.json._
 import com.datastax.driver.core.Session
@@ -21,6 +24,15 @@ class Serving(conn: Session)(implicit system: ActorSystem)
         complete {
           OK -> repo.findAll
         }
+      } ~
+      (get & path("ws")) {
+        handleWebsocketMessages(flow)
       }
     }
+
+  def flow = Flow.wrap(Sink.ignore, source.map(TextMessage.Strict))((_, _) => ())
+
+  def source = Source(1.second, 1.second, "tick").mapAsync(1) {
+    _ => repo.findAll map (_.toJson.compactPrint)
+  }
 }
