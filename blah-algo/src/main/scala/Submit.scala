a66597cd64838caa0986d04da965af865be5850a
Dennis Dietrich <d.dietrich84@gmail.com>
2015-08-17 20:29:37 +0200

    remove hadoop stuff

--- blah-algo/src/main/scala/Submit.scala
+++ blah-algo/src/main/scala/Submit.scala
@@ -1,7 +1,6 @@
 package blah.algo
 
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.hadoop.io.{BytesWritable, LongWritable}
 
 object Submit {
   def main(args: Array[String]) {
@@ -13,7 +12,7 @@ object Submit {
       .setAppName(args(0))
       .set("spark.cassandra.connection.host", "127.0.0.1")
     val sc = new SparkContext(conf)
-    val rdd = sc.textFile("hdfs://localhost:9000/user/dennis/blah/events.*")
+    val rdd = sc.textFile("hdfs://localhost:9000/blah/events.*")
     algo.train(rdd)
     sc.stop
   }
