b76d0fa62e238085b5de16a584aa2980279d8b43
Dennis Dietrich <d.dietrich84@gmail.com>
2015-08-22 11:36:49 +0200

    js-csp

--- blah-ui/src/js/dashboard.js
+++ blah-ui/src/js/dashboard.js
@@ -2,9 +2,9 @@ import {h} from 'virtual-dom'
 import component from './component'
 import pageviewWidget from './pageviews/widget'
 
-function render(conn, text) {
+function render(conn, model) {
   return h('div.dashboard', [
-    h('h1', 'Dashboard ' + text),
+    h('h1', 'Dashboard ' + model.count),
     h('div.widgets', [
       component(pageviewWidget, conn)
     ])
--- blah-ui/src/js/index.js
+++ blah-ui/src/js/index.js
@@ -1,19 +1,43 @@
+import 'babelify/polyfill'
 import {diff, patch} from 'virtual-dom'
 import createElement from 'virtual-dom/create-element'
+import csp from 'js-csp'
 import connect from './connection'
 import dashboard from './dashboard'
 
 var conn = connect('ws://localhost:8001/ws')
+var channel = csp.chan()
+var model = {count: 0}
 
-var tree = dashboard(conn, 'initial')
-var node = createElement(tree)
-document.body.appendChild(node)
+function update(model, action) {
+  switch (action.actionType) {
+    case 'incr':
+      model.count += 1
+      return model
+  }
+}
 
-var i = 0
+function renderLoop(model) {
+  var tree = dashboard(conn, 'initial')
+  var node = createElement(tree)
+  document.body.appendChild(node)
+
+  csp.go(function* () {
+    while (true) {
+      var action = yield csp.take(channel)
+      model = update(model, action)
+      var updated = dashboard(conn, model)
+        , patches = diff(tree, updated)
+      node = patch(node, patches)
+      tree = updated
+    }
+  });
+}
+
+renderLoop(model)
+
 setInterval(() => {
-  var updated = dashboard(conn, String(i))
-    , patches = diff(tree, updated)
-  node = patch(node, patches)
-  tree = updated
-  i ++
+  csp.go(function* (){
+    yield csp.put(channel, {actionType: 'incr'})
+  })
 }, 1000)
