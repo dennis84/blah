29d54787107395e65c071d1b91b385a7b3b13005
Dennis Dietrich <d.dietrich84@gmail.com>
2015-11-11 06:39:05 +0100

    streaming: send kafka messages

--- blah-streaming/src/main/scala/StreamingApp.scala
+++ blah-streaming/src/main/scala/StreamingApp.scala
@@ -1,11 +1,13 @@
 package blah.streaming
 
-import kafka.serializer.StringDecoder
+import scala.util.{Try, Success, Failure}
+import kafka.producer.KafkaProducer
+import kafka.serializer.{StringDecoder, StringEncoder}
 import org.apache.spark.SparkConf
-import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.kafka.KafkaUtils
 import com.typesafe.config.ConfigFactory
+import com.softwaremill.react.kafka.ProducerProperties
 import blah.algo._
 
 object StreamingApp {
@@ -14,6 +16,13 @@ object StreamingApp {
       "count" -> new CountAlgo)
     val algo = algos(args(0))
     val config = ConfigFactory.load()
+
+    val producer = KafkaProducer[String](ProducerProperties(
+      brokerList = config.getString("producer.broker.list"),
+      topic = "trainings",
+      clientId = "websocket",
+      encoder = new StringEncoder))
+
     val conf = new SparkConf()
       .setAppName(s"streaming-${args(0)}")
     conf.set("es.nodes", config.getString("elasticsearch.url"))
@@ -23,15 +32,21 @@ object StreamingApp {
     conf.set("es.update.script.params", "count:count")
 
     val ssc = new StreamingContext(conf, Seconds(10))
-    val stream = KafkaUtils.createStream(
-      ssc,
+    val stream = KafkaUtils.createStream(ssc,
       config.getString("consumer.zookeeper.connect"),
       config.getString("consumer.group.id"),
       Map("events" -> 1)
     ).map(_._2)
 
-    stream.foreachRDD(algo.train _)
+    stream.foreachRDD { rdd =>
+      algo.train(rdd)
+      Try(producer send args(0)) match {
+        case Success(_) => println("Successfully sent message")
+        case Failure(e) => println("Message could not be sent")
+      }
+    }
     stream.print()
+
     ssc.start()
     ssc.awaitTermination()
   }
