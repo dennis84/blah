343532ed599b37bc2b1d7249b77374ce421516b5
Dennis Dietrich <d.dietrich84@gmail.com>
2016-01-04 09:15:50 +0100

    daily visitors widget

--- blah-ui/src/js/dashboard.js
+++ blah-ui/src/js/dashboard.js
@@ -28,23 +28,28 @@ function render(model, chan, conn) {
     }, [
       widget(pie, model, chan, conn, {
         filterBy: [{
-          'prop': 'date.from',
-          'operator': 'gte',
-          'value': moment().subtract(1, 'year')
+          prop: 'date.from',
+          operator: 'gte',
+          value: moment().subtract(1, 'year')
         }],
         groupBy: ['date.year', 'user_agent.browser.family'],
         title: 'Browser Statistics Over a Year'
       }),
       widget(bar, model, chan, conn, {
         filterBy: [{
-          'prop': 'date.from',
-          'operator': 'gte',
-          'value': moment().subtract(1, 'day')
+          prop: 'date.from',
+          operator: 'gte',
+          value: moment().subtract(1, 'day')
         }],
         groupBy: ['date.hour'],
         title: 'Page Views in the past 24 hours',
         className: 'size-2of3'
       }),
+      widget(userCount, model, chan, conn, {filterBy: [{
+        prop: 'date.from',
+        operator: 'gte',
+        value: moment().subtract(1, 'day')
+      }], title: 'Number of visitors today'}),
       widget(userBar, model, chan, conn, {
         groupBy: ['country'],
         title: 'Visitors by Country'
@@ -63,7 +68,7 @@ function render(model, chan, conn) {
       widget(count, model, chan, conn, {filterBy: [{
         prop: 'page',
         operator: 'eq',
-        value: 'page1'
+        value: 'page-1'
       }], title: 'Page 1'}),
       widget(pie, model, chan, conn, {
         filterBy: [{
@@ -81,7 +86,7 @@ function render(model, chan, conn) {
       widget(count, model, chan, conn, {filterBy: [{
         prop: 'page',
         operator: 'eq',
-        value: 'page2'
+        value: 'page-2'
       }], title: 'Page 2'}),
       widget(recommendations, model, chan)
     ])
