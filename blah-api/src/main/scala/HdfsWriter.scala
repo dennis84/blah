a09f1018b4fdbb950eb8fea95ad3057d5b06e9df
Dennis Dietrich <d.dietrich84@gmail.com>
2016-01-10 12:21:33 +0100

    improve HdfsWriter

--- blah-api/src/main/scala/HdfsWriter.scala
+++ blah-api/src/main/scala/HdfsWriter.scala
@@ -4,7 +4,7 @@ import scala.concurrent.duration._
 import java.text.DecimalFormat
 import akka.actor._
 import spray.json._
-import com.github.nscala_time.time.Imports._
+import org.joda.time.DateTime
 import org.apache.hadoop.fs.{FileSystem, Path, FSDataOutputStream}
 import org.apache.hadoop.io.{SequenceFile, LongWritable, BytesWritable}
 import blah.core._
@@ -16,35 +16,33 @@ class HdfsWriter(
 ) extends Actor with ActorLogging {
   import context.dispatcher
 
-  context.system.scheduler.schedule(
-    DurationInt(10).seconds,
-    DurationInt(10).seconds,
-    self, HdfsWriter.Close)
-
   def receive = {
     case m@HdfsWriter.Write(e) =>
+      val timer = context.system.scheduler
+        .scheduleOnce(config.closeDelay, self, HdfsWriter.Close)
       val (writer, stream) = nextWriterAndStream
-      context become active(writer, stream, 0L)
-      self ! m
+      context become active(writer, stream, timer, 0L)
+      self forward m
   }
 
   def active(
     writer: SequenceFile.Writer,
     stream: FSDataOutputStream,
+    timer: Cancellable,
     bytesWritten: Long
   ): Receive = {
     case HdfsWriter.Write(e) =>
       log.debug("Write event")
       val value = new BytesWritable(e.toJson.compactPrint.getBytes)
       writer.append(new LongWritable(0L), value)
-      if(bytesWritten > config.batchSize) {
+      if(bytesWritten >= config.batchSize) {
         writer.hflush()
         writer.close()
         stream.close()
-        val (nextWriter, nextStream) = nextWriterAndStream
-        context become active(nextWriter, nextStream, 0L)
+        timer.cancel()
+        context become receive
       } else {
-        context become active(writer, stream, bytesWritten + value.getLength)
+        context become active(writer, stream, timer, bytesWritten + value.getLength)
       }
 
     case HdfsWriter.Close =>
@@ -52,11 +50,12 @@ class HdfsWriter(
       writer.hflush()
       writer.close()
       stream.close()
+      timer.cancel()
       context become receive
   }
 
   private def nextWriterAndStream() = {
-    val now = DateTime.now
+    val now = new DateTime
     val df = new DecimalFormat("00")
     val file = Seq(config.filePrefix, now.getMillis, config.fileSuffix) mkString ""
     val path = new Path(Seq(
@@ -81,4 +80,5 @@ case class HdfsWriterConfig(
   path: String = "/events/%Y/%m/%d",
   filePrefix: String = "events",
   fileSuffix: String = ".jsonl",
-  batchSize: Long = 1024 * 1024 * 256)
+  batchSize: Long = 1024 * 1024 * 256,
+  closeDelay: FiniteDuration = 10.seconds)
--- blah-api/src/test/scala/HdfsWriterSpec.scala
+++ blah-api/src/test/scala/HdfsWriterSpec.scala
@@ -7,7 +7,6 @@ import akka.actor._
 import akka.testkit._
 import akka.util.ByteString
 import spray.json._
-import com.github.nscala_time.time.Imports._
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.io.{SequenceFile, LongWritable, BytesWritable}
@@ -23,8 +22,6 @@ class HdfsWriterSpec
   val dfsConf = new Configuration()
   dfsConf.set("fs.file.impl", "org.apache.hadoop.fs.RawLocalFileSystem")
   val dfs = FileSystem.get(dfsConf)
-  val writer = system.actorOf(Props(new HdfsWriter(dfs, HdfsWriterConfig(
-    path = "target/test/events/%Y/%m/%d"))))
 
   before {
     dfs.delete(new Path("target/test/events"), true)
@@ -36,24 +33,27 @@ class HdfsWriterSpec
   }
 
   "The HdfsWriter" should "write" in {
-    val event1 = Event(UUID.randomUUID.toString, "view", DateTime.now)
-    val event2 = Event(UUID.randomUUID.toString, "view", DateTime.now)
-    val event3 = Event(UUID.randomUUID.toString, "view", DateTime.now)
+    val writer = system.actorOf(Props(new HdfsWriter(dfs, HdfsWriterConfig(
+      path = "target/test/events/%Y/%m/%d"))))
+
+    val event1 = Event(UUID.randomUUID.toString, "view")
+    val event2 = Event(UUID.randomUUID.toString, "view")
+    val event3 = Event(UUID.randomUUID.toString, "view")
 
     writer ! HdfsWriter.Write(event1)
-    expectNoMsg(DurationInt(2).seconds)
+    expectNoMsg(100.milliseconds)
 
     writer ! HdfsWriter.Write(event2)
-    expectNoMsg(DurationInt(2).seconds)
+    expectNoMsg(100.milliseconds)
 
     writer ! HdfsWriter.Close
-    expectNoMsg(DurationInt(2).seconds)
+    expectNoMsg(100.milliseconds)
 
     writer ! HdfsWriter.Write(event3)
-    expectNoMsg(DurationInt(2).seconds)
+    expectNoMsg(100.milliseconds)
 
     writer ! HdfsWriter.Close
-    expectNoMsg(DurationInt(2).seconds)
+    expectNoMsg(100.milliseconds)
 
     val statuses = dfs.globStatus(new Path("target/test/events/*/*/*/*"))
     statuses.length should be (2)
@@ -86,10 +86,14 @@ class HdfsWriterSpec
       .convertTo[Event] should be (event3)
   }
 
-  it should "close stream after 10 seconds" in {
-    val event1 = Event(UUID.randomUUID.toString, "view", DateTime.now)
+  it should "close stream after close delay" in {
+    val writer = system.actorOf(Props(new HdfsWriter(dfs, HdfsWriterConfig(
+      path = "target/test/events/%Y/%m/%d",
+      closeDelay = 100.milliseconds))))
+
+    val event1 = Event(UUID.randomUUID.toString, "view")
     writer ! HdfsWriter.Write(event1)
-    Thread sleep 10000
+    Thread sleep 200
 
     val statuses = dfs.globStatus(new Path("target/test/events/*/*/*/*"))
     statuses.length should be (1)
@@ -105,5 +109,42 @@ class HdfsWriterSpec
       .utf8String
       .parseJson
       .convertTo[Event] should be (event1)
+  }
+
+  it should "close stream when batchSize is full" in {
+    val event = Event(UUID.randomUUID.toString, "view")
+    val length = event.toJson.compactPrint.getBytes.length
+
+    val writer = system.actorOf(Props(new HdfsWriter(dfs, HdfsWriterConfig(
+      path = "target/test/events/%Y/%m/%d",
+      batchSize = length * 2,
+      closeDelay = 100.milliseconds))))
+
+    writer ! HdfsWriter.Write(event)
+    writer ! HdfsWriter.Write(event)
+    writer ! HdfsWriter.Write(event)
+    writer ! HdfsWriter.Write(event)
+
+    Thread sleep 500
+
+    val statuses = dfs.globStatus(new Path("target/test/events/*/*/*/*"))
+    statuses.length should be (2)
+
+    var reader = new SequenceFile.Reader(dfsConf,
+      SequenceFile.Reader.file(statuses(0).getPath))
+
+    var key = new LongWritable
+    var value = new BytesWritable
+    var nb = 0
+
+    while(reader.next(key, value)) (nb += 1)
+    nb should be (3)
+
+    reader = new SequenceFile.Reader(dfsConf,
+      SequenceFile.Reader.file(statuses(1).getPath))
+    nb = 0
+
+    while(reader.next(key, value)) (nb += 1)
+    nb should be (1)
   }
 }
