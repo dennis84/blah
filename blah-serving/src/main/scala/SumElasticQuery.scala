61d4c1ab650bfa3d446a30c5f4ac5ba9ecf623b3
Dennis Dietrich <d.dietrich84@gmail.com>
2016-01-24 11:09:43 +0100

    prop * count

--- blah-serving/src/main/scala/SumElasticQuery.scala
+++ blah-serving/src/main/scala/SumElasticQuery.scala
@@ -13,11 +13,15 @@ object SumElasticQuery {
     case Filter("date.to", "lte", value)   => f.lte("date", value)
   } reduceOption (_ merge _) getOrElse JsObject()
 
+  private def sum(prop: String) =
+    ("aggs" -> ("sum" -> ("sum" ->
+      ("script" -> s"doc['$prop'].value * doc['count'].value")
+    )))
+
   def apply(query: SumQuery): JsValue =
     q.term("collection", query.collection) merge (query match {
       case SumQuery(_, prop, Some(filters)) =>
-        filterBy(filters) merge a.sum("sum", prop)
-      case SumQuery(_, prop, _) =>
-        a.sum("sum", prop)
+        filterBy(filters) merge sum(prop)
+      case SumQuery(_, prop, _) => sum(prop)
     })
 }
