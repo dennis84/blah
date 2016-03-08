3c9687317c3b224f5686fca5e29d7cd047791543
Dennis Dietrich <d.dietrich84@gmail.com>
2016-03-08 17:09:09 +0100

    ui

--- blah-ui/src/js/ui/dashboard.js
+++ blah-ui/src/js/ui/dashboard.js
@@ -2,11 +2,11 @@ import {h} from 'virtual-dom'
 import Masonry from 'masonry-layout'
 import debounce from 'debounce'
 import {hook} from '../hook'
-import makeWidgets from './widgets'
+import * as widgets from './widgets'
 
-function render(model, chan, conn) {
+function container(title, ws) {
   return h('div.container', [
-    h('h1.center-h', 'Dashboard'),
+    h('h1.center-h', title),
     h('div.widgets', {
       hook: hook(debounce((node) => {
         if(undefined === node.masonry) {
@@ -18,7 +18,30 @@ function render(model, chan, conn) {
           node.masonry.layout()
         }
       }), 100)
-    }, makeWidgets(model, chan, conn))
+    }, ws)
+  ])
+}
+
+function render(model, chan, conn) {
+  return h('main', [
+    container('Pageviews', [
+      widgets.browserStats(model, chan, conn),
+      widgets.pageviews(model, chan, conn),
+      widgets.countAll(model, chan, conn),
+      widgets.platformStats(model, chan, conn),
+      widgets.pageviewDiff(model, chan, conn),
+      widgets.mobileOsStats(model, chan, conn)
+    ]),
+    container('User Stats', [
+      widgets.visitorsToday(model, chan, conn),
+      widgets.visitorsByCountry(model, chan, conn),
+      widgets.uniqueVisitors(model, chan, conn)
+    ]),
+    container('Misc', [
+      widgets.itemsFunnel(model, chan, conn),
+      widgets.totalRevenue(model, chan, conn),
+      widgets.recommendationsWidget(model, chan, conn)
+    ])
   ])
 }
 
--- blah-ui/src/js/ui/widgets.js
+++ blah-ui/src/js/ui/widgets.js
@@ -10,158 +10,190 @@ import userBar from './user/bar'
 import sum from './sum/sum'
 import funnel from './funnel/funnel'
 
-function makeWidgets(model, chan, conn) {
-  return [
-    // --------------------------------------------------------------
-    // Pie Chart: Browser Statistics Over a Year
-    // --------------------------------------------------------------
-    widget(pie, model, chan, conn, {
-      collection: 'pageviews',
-      filterBy: [{
-        prop: 'date.from',
-        operator: 'gte',
-        value: moment().subtract(1, 'year')
-      }],
-      groupBy: ['date.year', 'user_agent.browser.family'],
-      title: 'Browser Statistics Over a Year'
-    }),
-
-    // --------------------------------------------------------------
-    // Bar Chart: Page Views in the past 24 hours
-    // --------------------------------------------------------------
-    widget(bar, model, chan, conn, {
-      collection: 'pageviews',
-      filterBy: [{
-        prop: 'date.from',
-        operator: 'gte',
-        value: moment().subtract(1, 'day')
-      }],
-      groupBy: ['date.hour'],
-      title: 'Page Views in the past 24 hours',
-      className: 'size-2of3'
-    }),
-
-    // --------------------------------------------------------------
-    // Count: Number of Visitors Today
-    // --------------------------------------------------------------
-    widget(userCount, model, chan, conn, {
-      filterBy: [{
-        prop: 'date.from',
-        operator: 'gte',
-        value: moment().subtract(1, 'day')
-      }],
-      title: 'Number of Visitors Today'
-    }),
-
-    // --------------------------------------------------------------
-    // Bar Chart: Visitors by Country
-    // --------------------------------------------------------------
-    widget(userBar, model, chan, conn, {
-      groupBy: ['country'],
-      title: 'Visitors by Country'
-    }),
-
-    // --------------------------------------------------------------
-    // Count: Unique Visitors
-    // --------------------------------------------------------------
-    widget(userCount, model, chan, conn, {title: 'Unique Visitors'}),
-
-    // --------------------------------------------------------------
-    // Count: All Page Views
-    // --------------------------------------------------------------
-    widget(count, model, chan, conn, {
-      collection: 'pageviews',
-      title: 'All'
-    }),
-
-    // --------------------------------------------------------------
-    // Pie Chart: Platform Statistics
-    // --------------------------------------------------------------
-    widget(pie, model, chan, conn, {
-      collection: 'pageviews',
-      filterBy: [{
-        prop: 'date.from',
-        operator: 'gte',
-        value: moment().subtract(1, 'year')
-      }],
-      groupBy: ['date.year', 'user_agent.platform'],
-      title: 'Platform Statistics'
-    }),
-
-    // --------------------------------------------------------------
-    // Count Diff: Page View Difference Between Yesterday and Today
-    // --------------------------------------------------------------
-    widget(countDiff, model, chan, conn, {
-      collection: 'pageviews',
-      percentage: true,
-      progressBar: true,
-      from: {
-        filterBy: [{
-          prop: 'date.from',
-          operator: 'gte',
-          value: moment().subtract(1, 'day').startOf('day')
-        }, {
-          prop: 'date.to',
-          operator: 'lte',
-          value: moment().subtract(1, 'day').endOf('hour')
-        }]
-      },
-      to: {
-        filterBy: [{
-          prop: 'date.from',
-          operator: 'gte',
-          value: moment().startOf('day')
-        }, {
-          prop: 'date.to',
-          operator: 'lte',
-          value: moment().endOf('hour')
-        }]
-      },
-      title: `Difference between ${moment().subtract(1, 'days').endOf('hour').calendar()}
-              and Today ${moment().endOf('hour').calendar()}`
-    }),
-
-    // --------------------------------------------------------------
-    // Pie Chart: Mobile Operating Statistics
-    // --------------------------------------------------------------
-    widget(pie, model, chan, conn, {
-      collection: 'pageviews',
-      filterBy: [{
-        prop: 'date.from',
-        operator: 'gte',
-        value: moment().subtract(1, 'year')
-      }, {
-        prop: 'user_agent.platform',
-        operator: 'eq',
-        value: 'mobile'
-      }],
-      groupBy: ['date.year', 'user_agent.os.family'],
-      title: 'Mobile Operating Systems'
-    }),
-
-    // --------------------------------------------------------------
-    // Sum: Total Revenue
-    // --------------------------------------------------------------
-    widget(sum, model, chan, conn, {
-      collection: 'purchases',
-      template: '$ {value}',
-      prop: 'price',
-      title: 'Total Revenue'
-    }),
-
-    // --------------------------------------------------------------
-    // Recommendations
-    // --------------------------------------------------------------
-    widget(recommendations, model, chan),
-
-    // --------------------------------------------------------------
-    // Funnel
-    // --------------------------------------------------------------
-    widget(funnel, model, chan, conn, {
-      name: 'items',
-      title: 'Items 1-3 Funnel'
-    })
-  ]
+/**
+ * Pie Chart: Browser Statistics Over a Year
+ */
+function browserStats(model, chan, conn) {
+  return widget(pie, model, chan, conn, {
+    collection: 'pageviews',
+    filterBy: [{
+      prop: 'date.from',
+      operator: 'gte',
+      value: moment().subtract(1, 'year')
+    }],
+    groupBy: ['date.year', 'user_agent.browser.family'],
+    title: 'Browser Statistics Over a Year'
+  })
 }
 
-export default makeWidgets
+/**
+ * Bar Chart: Page Views in the past 24 hours
+ */
+function pageviews(model, chan, conn) {
+  return widget(bar, model, chan, conn, {
+    collection: 'pageviews',
+    filterBy: [{
+      prop: 'date.from',
+      operator: 'gte',
+      value: moment().subtract(1, 'day')
+    }],
+    groupBy: ['date.hour'],
+    title: 'Page Views in the past 24 hours',
+    className: 'size-2of3'
+  })
+}
+
+/**
+ * Count: Number of Visitors Today
+ */
+function visitorsToday(model, chan, conn) {
+  return widget(userCount, model, chan, conn, {
+    filterBy: [{
+      prop: 'date.from',
+      operator: 'gte',
+      value: moment().subtract(1, 'day')
+    }],
+    title: 'Number of Visitors Today'
+  })
+}
+
+/**
+ * Bar Chart: Visitors by Country
+ */
+function visitorsByCountry(model, chan, conn) {
+  return widget(userBar, model, chan, conn, {
+    groupBy: ['country'],
+    title: 'Visitors by Country'
+  })
+}
+
+/**
+ * Count: Unique Visitors
+ */
+function uniqueVisitors(model, chan, conn) {
+  return widget(userCount, model, chan, conn, {title: 'Unique Visitors'})
+}
+
+/**
+ * Count: All Page Views
+ */
+function countAll(model, chan, conn) {
+  return widget(count, model, chan, conn, {
+    collection: 'pageviews',
+    title: 'All'
+  })
+}
+
+/**
+ * Pie Chart: Platform Statistics
+ */
+function platformStats(model, chan, conn) {
+  return widget(pie, model, chan, conn, {
+    collection: 'pageviews',
+    filterBy: [{
+      prop: 'date.from',
+      operator: 'gte',
+      value: moment().subtract(1, 'year')
+    }],
+    groupBy: ['date.year', 'user_agent.platform'],
+    title: 'Platform Statistics'
+  })
+}
+
+/**
+ * Count Diff: Page View Difference Between Yesterday and Today
+ */
+function pageviewDiff(model, chan, conn) {
+  return widget(countDiff, model, chan, conn, {
+    collection: 'pageviews',
+    percentage: true,
+    from: {
+      filterBy: [{
+        prop: 'date.from',
+        operator: 'gte',
+        value: moment().subtract(1, 'day').startOf('day')
+      }, {
+        prop: 'date.to',
+        operator: 'lte',
+        value: moment().subtract(1, 'day').endOf('hour')
+      }]
+    },
+    to: {
+      filterBy: [{
+        prop: 'date.from',
+        operator: 'gte',
+        value: moment().startOf('day')
+      }, {
+        prop: 'date.to',
+        operator: 'lte',
+        value: moment().endOf('hour')
+      }]
+    },
+    title: `Difference between ${moment().subtract(1, 'days').endOf('hour').calendar()}
+            and Today ${moment().endOf('hour').calendar()}`
+  })
+}
+
+/**
+ * Pie Chart: Mobile Operating Statistics
+ */
+function mobileOsStats(model, chan, conn) {
+  return widget(pie, model, chan, conn, {
+    collection: 'pageviews',
+    filterBy: [{
+      prop: 'date.from',
+      operator: 'gte',
+      value: moment().subtract(1, 'year')
+    }, {
+      prop: 'user_agent.platform',
+      operator: 'eq',
+      value: 'mobile'
+    }],
+    groupBy: ['date.year', 'user_agent.os.family'],
+    title: 'Mobile Operating Systems'
+  })
+}
+
+/**
+ * Sum: Total Revenue
+ */
+function totalRevenue(model, chan, conn) {
+  return widget(sum, model, chan, conn, {
+    collection: 'purchases',
+    template: '$ {value}',
+    prop: 'price',
+    title: 'Total Revenue'
+  })
+}
+
+/**
+ * Recommendations
+ */
+function recommendationsWidget(model, chan, conn) {
+  return widget(recommendations, model, chan)
+}
+
+/**
+ * Funnel
+ */
+function itemsFunnel(model, chan, conn) {
+  return widget(funnel, model, chan, conn, {
+    name: 'items',
+    title: 'Items 1-3 Funnel'
+  })
+}
+
+export {
+ browserStats,
+ pageviews,
+ visitorsToday,
+ visitorsByCountry,
+ uniqueVisitors,
+ countAll,
+ platformStats,
+ pageviewDiff,
+ mobileOsStats,
+ totalRevenue,
+ recommendationsWidget,
+ itemsFunnel
+}
