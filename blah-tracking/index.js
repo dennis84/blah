c29c12f9bd997743f2f26a7bd9d4b8a8c14ac8f5
Dennis Dietrich <d.dietrich84@gmail.com>
2016-04-10 07:59:30 +0200

    add tracking.js

---
+++ blah-tracking/index.js
@@ -0,0 +1,38 @@
+var xhr = require('xhr')
+var API_URL = 'http://api.marathon.mesos:8000'
+
+/**
+ * Tracking API.
+ *
+ * ```
+ * track('pageviews', {
+ *   'item': 'home',
+ *   'title': 'Visited page',
+ *   'user': 'username'
+ * })
+ * ```
+ *
+ * @param {String} collection
+ * @param {Object} params
+ *
+ * @return {Promise}
+ */
+function track(collection, params) {
+  return new Promise(function(resolve, reject) {
+    xhr({
+      uri: API_URL + '/events/' + collection,
+      method: 'POST',
+      json: params,
+      timeout: 10000,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }, function(err, resp, body) {
+      if(200 !== resp.statusCode) reject(resp)
+      resolve(body)
+    })
+  })
+}
+
+module.exports = track
