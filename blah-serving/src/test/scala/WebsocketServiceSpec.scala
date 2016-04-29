848faa329e21ceb971de74cdef8be4dd61fe8940
Dennis Dietrich <d.dietrich84@gmail.com>
2016-04-29 07:28:09 +0200

    test websocket service

---
+++ blah-serving/src/test/scala/WebsocketServiceSpec.scala
@@ -0,0 +1,49 @@
+package blah.serving
+
+import scala.concurrent.duration._
+import org.scalatest._
+import akka.actor._
+import akka.stream.testkit.scaladsl._
+import akka.http.scaladsl.model.ws._
+import akka.http.scaladsl.testkit._
+
+class WebsocketServiceSpec
+  extends FlatSpec
+  with Matchers
+  with ScalatestRouteTest {
+
+  "The WebsocketHub" should "send" in {
+    implicit val system = ActorSystem()
+
+    val room = new WebsocketRoom(system)
+    val hub = system.actorOf(Props(new WebsocketHub(room)))
+    val service = new WebsocketService(room)
+    val client = WSProbe()
+
+    val (source, sink) = service.flow.runWith(
+      TestSource.probe[Message],
+      TestSink.probe[Message])
+
+    source.sendNext(TextMessage("Ping"))
+    sink.request(1)
+    sink.expectNoMsg(100.millis)
+
+    List(
+      """count@{"foo": "bar"}""",
+      """user@{"foo": "bar"}""",
+      """funnel@{"foo": "bar"}""",
+      """similarity@{"foo": "bar"}"""
+    ) foreach { x =>
+      val parts = x.split("@", 2)
+      hub ! (parts(0), parts(1))
+      sink.request(1)
+      sink.expectNext(TextMessage(x))
+    }
+
+    hub ! ("foo", "bar")
+    sink.request(1)
+    sink.expectNoMsg(100.millis)
+
+    system.terminate()
+  }
+}
