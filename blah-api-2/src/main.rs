488b8c71aa8c2f41268944540ced9132357e8650
Dennis Dietrich <d.dietrich84@gmail.com>
2016-05-21 13:13:21 +0200

    wip

--- blah-api-2/src/main.rs
+++ blah-api-2/src/main.rs
@@ -1,14 +1,70 @@
 extern crate hyper;
+extern crate rustc_serialize;
+extern crate uuid;
+extern crate kafka;
 
-use hyper::server::{Request, Response};
+use hyper::{Get, Post};
+use hyper::server::{Server, Request, Response};
+use hyper::header::{ContentType};
+use hyper::uri::RequestUri::AbsolutePath;
+use rustc_serialize::json::{self, Json};
+use uuid::Uuid;
+use std::io::Read;
+use kafka::producer::{Producer, Record};
 
-static PHRASE: &'static [u8] = b"Hello World!";
+#[derive(RustcEncodable)]
+struct Event {
+    id: Uuid,
+    collection: String,
+    props: Json
+}
 
-fn hello(_: Request, res: Response) {
-    res.send(PHRASE).unwrap();
+fn echo(mut req: Request, mut res: Response) {
+    let mut producer =
+        Producer::from_hosts(vec!("localhost:9092".to_owned()))
+            .with_ack_timeout(1000)
+            .with_required_acks(1)
+            .create()
+            .unwrap();
+
+    match req.uri.clone() {
+        AbsolutePath(ref path) => match (&req.method, &path[..]) {
+            (&Post, path) if path.starts_with("/events/") => {
+                let (_, collection) = path.split_at(8);
+				let mut body = String::new();
+                req.read_to_string(&mut body).unwrap();
+                let event = Event {
+                    id: Uuid::new_v4(),
+                    collection: collection.to_string(),
+                    props: Json::from_str(&body).unwrap()
+                };
+
+                let encoded = json::encode(&event).unwrap();
+                producer.send(&Record::from_value("test", encoded.as_bytes())).unwrap();
+
+                res.headers_mut().set(ContentType::json());
+                res.send(b"{\"message\": \"Event successfully created.\"}").unwrap();
+                return;
+            },
+            (&Get, "/") => {
+                res.headers_mut().set(ContentType::json());
+                res.send(b"{\"message\": \"Ok\"}").unwrap();
+                return;
+            },
+            _ => {
+                *res.status_mut() = hyper::NotFound;
+                return;
+            }
+        },
+        _ => {
+            return;
+        }
+    };
 }
 
 fn main() {
-    let _ = hyper::Server::http("0.0.0.0:8000").unwrap().handle(hello);
-    println!("Listening on http://0.0.0.0:8000");
+    Server::http("0.0.0.0:8000")
+        .unwrap()
+        .handle(echo)
+        .unwrap();
 }
