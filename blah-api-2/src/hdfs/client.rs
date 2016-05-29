b2eb9a314bb78ed15fb6e2c21a86caa649a17810
Dennis Dietrich <d.dietrich84@gmail.com>
2016-05-29 10:59:05 +0200

    server defaults

--- blah-api-2/src/hdfs/client.rs
+++ blah-api-2/src/hdfs/client.rs
@@ -14,6 +14,8 @@ use super::super::proto::ClientNamenodeProtocol::{
     CompleteResponseProto,
     GetFileInfoRequestProto,
     GetFileInfoResponseProto,
+    GetServerDefaultsRequestProto,
+    GetServerDefaultsResponseProto,
 };
 
 pub trait Client {
@@ -21,10 +23,12 @@ pub trait Client {
         -> ProtobufResult<GetFileInfoResponseProto>;
     fn mkdir(&mut self, src: &str)
         -> ProtobufResult<MkdirsResponseProto>;
-    fn rmdir(&mut self, src: &str)
+    fn rm(&mut self, src: &str)
         -> ProtobufResult<DeleteResponseProto>;
     fn touch(&mut self, src: &str)
         -> ProtobufResult<CompleteResponseProto>;
+    fn get_server_defaults(&mut self)
+        -> ProtobufResult<GetServerDefaultsResponseProto>;
     fn create(&mut self, src: &str)
         -> Writer;
 }
@@ -53,7 +57,7 @@ impl Client for Channel {
         })
     }
 
-    fn rmdir(&mut self, src: &str) -> ProtobufResult<DeleteResponseProto> {
+    fn rm(&mut self, src: &str) -> ProtobufResult<DeleteResponseProto> {
         let mut req = DeleteRequestProto::new();
         req.set_src(src.to_string());
         req.set_recursive(false);
@@ -64,6 +68,8 @@ impl Client for Channel {
     }
 
     fn touch(&mut self, src: &str) -> ProtobufResult<CompleteResponseProto> {
+        let defaults = try!(self.get_server_defaults());
+        let defaults = defaults.get_serverDefaults();
         let mut perm = FsPermissionProto::new();
         perm.set_perm(644);
         let mut req = CreateRequestProto::new();
@@ -72,17 +78,26 @@ impl Client for Channel {
         req.set_clientName("blah".to_string());
         req.set_createFlag(0x01);
         req.set_createParent(false);
-        req.set_replication(1);
-        req.set_blockSize(134217728);
-        let resp: CreateResponseProto = try!(self.send(&Request {
+        req.set_replication(defaults.get_replication());
+        req.set_blockSize(defaults.get_blockSize());
+        try!(self.send::<CreateResponseProto>(&Request {
             method: "create",
-            message: &req
+            message: &req,
         }));
 
         let mut req = CompleteRequestProto::new();
         req.set_src(src.to_string());
         req.set_clientName("blah".to_string());
         self.send(&Request {method: "complete", message: &req})
+    }
+
+    fn get_server_defaults(&mut self)
+            -> ProtobufResult<GetServerDefaultsResponseProto> {
+        let req = GetServerDefaultsRequestProto::new();
+        self.send(&Request {
+            method: "getServerDefaults",
+            message: &req,
+        })
     }
 
     fn create(&mut self, src: &str) -> Writer {
--- blah-api-2/src/hdfs/test.rs
+++ blah-api-2/src/hdfs/test.rs
@@ -4,18 +4,10 @@ use super::client::{Client};
 #[test]
 fn test() {
     let mut chan = ChannelFactory::new("127.0.0.1:8020").unwrap();
-    match chan.mkdir("/test") {
-        Ok(resp) => { println!("{:?}", resp) }
-        Err(e) => { println!("{:?}", e) }
-    }
-
-    match chan.mkdir("/test2") {
-        Ok(resp) => { println!("{:?}", resp) }
-        Err(e) => { println!("{:?}", e) }
-    }
-
-    // chan.rmdir("/test");
-    // chan.touch("/foo.txt");
+    chan.mkdir("/test").unwrap();
+    chan.rm("/test").unwrap();
+    chan.touch("/foo.txt").unwrap();
+    chan.rm("/foo.txt").unwrap();
     // let mut writer = chan.create("/test.txt");
     // writer.write(b"hello");
     // writer.write(b"world");
