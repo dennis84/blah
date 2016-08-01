b8894901f78d5f61791ffbd6be0776608fd92bc6
Dennis Dietrich <d.dietrich84@gmail.com>
2016-08-01 09:52:23 +0200

    cleanup

--- blah-algo/src/test/scala/RecommendationAlgoSpec.scala
+++ blah-algo/src/test/scala/RecommendationAlgoSpec.scala
@@ -54,53 +54,53 @@ class RecommendationAlgoSpec extends FlatSpec with Matchers with SparkFun {
     user2.items(1).item should be ("page2")
   }
 
-  // it should "filter by collection" in withSparkContext { ctx =>
-  //   val algo = new RecommendationAlgo
-  //   val input = ctx.sparkContext.parallelize(List(
-  //     Event("1", "buy", props = Map(
-  //       "item" -> JsString("product1"),
-  //       "user" -> JsString("user1")
-  //     )).toJson.compactPrint,
-  //     Event("2", "buy", props = Map(
-  //       "item" -> JsString("product2"),
-  //       "user" -> JsString("user1")
-  //     )).toJson.compactPrint,
-  //     Event("3", "buy", props = Map(
-  //       "item" -> JsString("product1"),
-  //       "user" -> JsString("user2")
-  //     )).toJson.compactPrint,
-  //     Event("4", "view", props = Map(
-  //       "item" -> JsString("page1"),
-  //       "user" -> JsString("user2")
-  //     )).toJson.compactPrint))
+  it should "filter by collection" in withSparkContext { ctx =>
+    val algo = new RecommendationAlgo
+    val input = ctx.sparkContext.parallelize(List(
+      Event("1", "buy", props = Map(
+        "item" -> JsString("product1"),
+        "user" -> JsString("user1")
+      )).toJson.compactPrint,
+      Event("2", "buy", props = Map(
+        "item" -> JsString("product2"),
+        "user" -> JsString("user1")
+      )).toJson.compactPrint,
+      Event("3", "buy", props = Map(
+        "item" -> JsString("product1"),
+        "user" -> JsString("user2")
+      )).toJson.compactPrint,
+      Event("4", "view", props = Map(
+        "item" -> JsString("page1"),
+        "user" -> JsString("user2")
+      )).toJson.compactPrint))
 
-  //   val output = algo.train(input, ctx, Array("--collection", "buy"))
-  //   val docs = output.collect.toList
+    val output = algo.train(input, ctx, Array("--collection", "buy"))
+    val docs = output.collect.toList
 
-  //   val user1 = docs.find(_._2.user == "user1").get._2
-  //   val user2 = docs.find(_._2.user == "user2").get._2
+    val user1 = docs.find(_.user == "user1").get
+    val user2 = docs.find(_.user == "user2").get
 
-  //   user1.items.length should be (0)
-  //   user1.collection should be (Some("buy"))
-  //   user2.items.length should be (1)
-  // }
+    user1.items.length should be (0)
+    user1.collection should be (Some("buy"))
+    user2.items.length should be (1)
+  }
 
-  // it should "not fail with wrong data" in withSparkContext { ctx =>
-  //   val algo = new RecommendationAlgo
-  //   val input = ctx.sparkContext.parallelize(List(
-  //     Event("1", "foo", props = Map(
-  //       "x" -> JsString("bar")
-  //     )).toJson.compactPrint,
-  //     Event("1", "foo", props = Map(
-  //       "x" -> JsString("baz")
-  //     )).toJson.compactPrint
-  //   ))
+  it should "not fail with wrong data" in withSparkContext { ctx =>
+    val algo = new RecommendationAlgo
+    val input = ctx.sparkContext.parallelize(List(
+      Event("1", "foo", props = Map(
+        "x" -> JsString("bar")
+      )).toJson.compactPrint,
+      Event("1", "foo", props = Map(
+        "x" -> JsString("baz")
+      )).toJson.compactPrint
+    ))
 
-  //   val thrown = intercept[IllegalArgumentException] {
-  //     algo.train(input, ctx, Array.empty[String])
-  //   }
+    val thrown = intercept[IllegalArgumentException] {
+      algo.train(input, ctx, Array.empty[String])
+    }
 
-  //   thrown.getMessage should be
-  //     ("requirement failed: view events cannot be empty")
-  // }
+    thrown.getMessage should be
+      ("requirement failed: view events cannot be empty")
+  }
 }
