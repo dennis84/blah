34a85f6995c98fafb9156284ed0c69ba4840421b
Dennis Dietrich <d.dietrich84@gmail.com>
2016-08-05 08:18:20 +0200

    hamburger menu

--- blah-ui/src/js/ui/nav.js
+++ blah-ui/src/js/ui/nav.js
@@ -2,17 +2,27 @@ import {h} from 'virtual-dom'
 import {theme} from '../ctrl'
 
 function render(model, update, conn, storage) {
-  return h('nav.level.navbar', [
-    h('div.level-left', [
-      h('a.button.is-link', {href: '#/pageviews'}, 'Pageviews'),
-      h('a.button.is-link', {href: '#/user'}, 'User Stats'),
-      h('a.button.is-link', {href: '#/misc'}, 'Misc'),
-      h('a.button.is-link', {href: '#/people'}, 'People'),
-      h('a.button.is-link', {href: '#/funnel'}, 'Funnel'),
-      h('a.button.is-link', {href: '#/segmentation'}, 'Segmentation'),
-      h('a.button.is-danger.is-outlined', {href: '#/world-map'}, 'Map')
+  return h('nav.level.nav', [
+    h('span.nav-toggle', {
+      onclick: e => {
+        var elem = e.currentTarget
+        var display = elem.classList.contains('is-active') ? 'none' : 'block'
+        elem.classList.toggle('is-active')
+        elem.parentNode.querySelector('.nav-menu').style.display = display
+      }
+    }, [
+      h('span'), h('span'), h('span')
     ]),
-    h('div.level-right', [
+    h('div.level-left.nav-menu', [
+      h('a.nav-item.button.is-link', {href: '#/pageviews'}, 'Pageviews'),
+      h('a.nav-item.button.is-link', {href: '#/user'}, 'User Stats'),
+      h('a.nav-item.button.is-link', {href: '#/misc'}, 'Misc'),
+      h('a.nav-item.button.is-link', {href: '#/people'}, 'People'),
+      h('a.nav-item.button.is-link', {href: '#/funnel'}, 'Funnel'),
+      h('a.nav-item.button.is-link', {href: '#/segmentation'}, 'Segmentation'),
+      h('a.nav-item.button.is-danger.is-outlined', {href: '#/world-map'}, 'Map')
+    ]),
+    h('div.level-right.nav-menu', [
       h('a.button.is-link', {
         onclick: node => update('theme', storage)
       }, h('i.material-icons', 'color_lens'))
