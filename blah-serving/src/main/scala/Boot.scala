a147c5b285f1cce37b6fef1c193b4a9267f467f4
Dennis Dietrich <d.dietrich84@gmail.com>
2016-09-23 08:05:54 +0200

    send kafka messages from topic "events" to client

--- blah-serving/src/main/scala/Boot.scala
+++ blah-serving/src/main/scala/Boot.scala
@@ -42,9 +42,12 @@ object Boot extends App
   Try(env.consumer) match {
     case Failure(e) => log.warning("Unable to connect to zookeeper.")
     case Success(c) => c runForeach { x =>
-      (x.value.split("@", 2)).toList match {
-        case e +: m +: Nil => env.websocketHub ! (e, m)
-        case _ => println("Could not handle message")
+      x.topic match {
+        case "events" => env.websocketHub ! ("events", x.value)
+        case "trainings" => (x.value.split("@", 2)).toList match {
+          case e +: m +: Nil => env.websocketHub ! (e, m)
+          case _ => println("Could not handle message")
+        }
       }
     }
   }
--- blah-serving/src/main/scala/WebsocketHub.scala
+++ blah-serving/src/main/scala/WebsocketHub.scala
@@ -12,5 +12,7 @@ class WebsocketHub(room: WebsocketRoom) extends Actor {
       room.send("user", message)
     case ("funnel", message: String) =>
       room.send("funnel", message)
+    case ("events", message: String) =>
+      room.send("events", message)
   }
 }
