7046e0795093bcd8a1e3568e70b1bd76d1710264
Dennis Dietrich <d.dietrich84@gmail.com>
2016-10-09 10:55:39 +0200

    stuff

--- blah-autoscale/src/server.rs
+++ blah-autoscale/src/server.rs
@@ -1,10 +1,10 @@
 
-use std::io::{self};
-use mio::*;
-use mio::tcp::{TcpListener, TcpStream};
-use mio::channel::{channel, Sender, Receiver};
+use std::io::{self, Write, Read};
 use std::collections::HashMap;
-use std::io::{Write};
+use std::str::from_utf8;
+use mio::*;
+use mio::tcp::{TcpListener, TcpStream, Shutdown};
+use mio::channel::{channel, Sender, Receiver};
 
 const MESSAGE: Token = Token(0);
 const SERVER: Token = Token(1);
@@ -50,7 +50,9 @@ impl Server {
                     SERVER => self.handle_conn().unwrap_or_else(|e| {
                         debug!("Error during connection handling: {}", e);
                     }),
-                    _ => {},
+                    Token(id) => self.handle_req(event, id).unwrap_or_else(|e| {
+                        debug!("Error during req: {}", e);
+                    }),
                 }
             }
         }
@@ -75,11 +77,41 @@ impl Server {
 
     fn handle_message(&mut self) -> io::Result<()> {
         if let Ok(msg) = self.rx.try_recv() {
+            debug!("Nb clients: {}", self.clients.len());
             let data = format!("data: {}\n\n", msg);
             let bytes = data.as_bytes();
             for (_, mut client) in &self.clients {
                 try!(client.write(bytes));
             }
+        }
+
+        Ok(())
+    }
+
+    fn handle_req(&mut self, event: Event, id: usize) -> io::Result<()> {
+        if event.kind().is_readable() {
+            if let Some(mut stream) = self.clients.get(&id) {
+                let mut buf = [0u8; 1024];
+                match stream.read(&mut buf) {
+                    Err(_) => {},
+                    Ok(bytes_read) => {
+                        let vec = buf[..bytes_read].to_vec();
+                        match from_utf8(&vec) {
+                            Ok(x) => println!("{}", x),
+                            Err(_) => {},
+                        }
+                    },
+                }
+            }
+        }
+
+        if event.kind().is_error() || event.kind().is_hup() {
+            if let Some(stream) = self.clients.get_mut(&id) {
+                self.poll.deregister(stream).unwrap();
+                try!(stream.shutdown(Shutdown::Both));
+            }
+
+            self.clients.remove(&id);
         }
 
         Ok(())
