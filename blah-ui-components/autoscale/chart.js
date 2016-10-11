3f58b7509d4de7e1678d4629b4fc150c717f0fbf
Dennis Dietrich <d.dietrich84@gmail.com>
2016-10-11 11:15:34 +0200

    donut

--- blah-ui-components/autoscale/chart.js
+++ blah-ui-components/autoscale/chart.js
@@ -1,16 +1,20 @@
 var d3 = require('d3')
 
 var nodes = [
-  {id: 'Foo', group: 1},
-  {id: 'Bar', group: 1},
-  {id: 'Baz', group: 1},
-  {id: 'Qux', group: 1},
+  {id: 'Foo', cpu_usage: 0, mem_usage: 0},
+  {id: 'Bar', cpu_usage: 20, mem_usage: 20},
+  {id: 'Baz', cpu_usage: 30, mem_usage: 60},
+  {id: 'Qux 1', cpu_usage: 70, mem_usage: 30},
+  {id: 'Qux 2', cpu_usage: 20, mem_usage: 10},
+  {id: 'Qux 3', cpu_usage: 20, mem_usage: 10}
 ]
 
 var links = [
-  {source: 'Foo', target: 'Bar', value: 1},
-  {source: 'Foo', target: 'Baz', value: 1},
-  {source: 'Foo', target: 'Qux', value: 1},
+  {source: 'Foo', target: 'Bar'},
+  {source: 'Foo', target: 'Baz'},
+  {source: 'Foo', target: 'Qux 1'},
+  {source: 'Qux 1', target: 'Qux 2'},
+  {source: 'Qux 1', target: 'Qux 3'}
 ]
 
 function chart(node) {
@@ -20,6 +24,9 @@ function chart(node) {
   var simulation = d3.forceSimulation()
     .force('charge', d3.forceManyBody())
     .force('center', d3.forceCenter(width / 2, height / 2))
+    .force("collide", d3.forceCollide().radius(function(d) {
+      return 30
+    }).iterations(2))
     .force('link', d3.forceLink().distance(80).id(function(d) {
       return d.id
     }))
@@ -29,7 +36,7 @@ function chart(node) {
       .attr('width', width)
       .attr('height', height)
 
-  var link = svg.append('g')
+  var linkSvg = svg.append('g')
     .attr('class', 'links')
     .selectAll('line')
     .data(links)
@@ -39,20 +46,69 @@ function chart(node) {
           return Math.sqrt(d.value)
         })
 
-  var node = svg.append('g')
+  var nodeSvg = svg.append('g')
     .attr('class', 'nodes')
-    .selectAll('circle')
+    .selectAll('.node')
     .data(nodes)
-    .enter().append('circle')
-      .attr('r', 30)
+    .enter()
+      .append('g')
+      .attr('class', 'node')
       .call(d3.drag()
         .on('start', dragstarted)
         .on('drag', dragged)
         .on('end', dragended))
+      .each(function(d, i) {
+        var cpuUsageColor = d3.scaleOrdinal()
+          .range(Array.apply(null, {length: 26}).map(function(x, i) {
+            return String.fromCharCode(97 + i)
+          }))
+        var cpuUsageArc = d3.arc()
+          .outerRadius(30)
+          .innerRadius(24)
+        var cpuUsagePie = d3.pie()
+          .sort(null)
+          .value(function(d) {
+            return d
+          })
 
-  node.append('title').text(function(d) {
-    return d.id
-  })
+
+        var memUsageColor = d3.scaleOrdinal()
+          .range(Array.apply(null, {length: 26}).map(function(x, i) {
+            return String.fromCharCode(97 + i)
+          }))
+        var memUsageArc = d3.arc()
+          .outerRadius(25)
+          .innerRadius(20)
+        var memUsagePie = d3.pie()
+          .sort(null)
+          .value(function(d) {
+            return d
+          })
+
+        var elem = d3.select(this)
+
+        elem.selectAll('.arc-cpu-usage')
+          .data(cpuUsagePie([d.cpu_usage, 100 - d.cpu_usage]))
+          .enter().append('g')
+          .attr('class', function(d) {
+            return 'arc arc-cpu-usage arc-' + cpuUsageColor(d.data)
+          })
+          .append('path')
+          .attr('d', cpuUsageArc)
+
+        elem.selectAll('.mem-cpu-usage')
+          .data(cpuUsagePie([d.mem_usage, 100 - d.mem_usage]))
+          .enter().append('g')
+          .attr('class', function(d) {
+            return 'arc mem-cpu-usage arc-' + memUsageColor(d.data)
+          })
+          .append('path')
+          .attr('d', memUsageArc)
+
+        elem
+          .append('circle')
+          .attr('r', 20)
+      })
 
   simulation
     .nodes(nodes)
@@ -62,15 +118,15 @@ function chart(node) {
     .links(links)
 
   function ticked() {
-    link
+    linkSvg
       .attr('x1', function(d) { return d.source.x })
       .attr('y1', function(d) { return d.source.y })
       .attr('x2', function(d) { return d.target.x })
       .attr('y2', function(d) { return d.target.y })
 
-    node
-      .attr('cx', function(d) { return d.x })
-      .attr('cy', function(d) { return d.y })
+    nodeSvg.attr('transform', function(d) {
+      return 'translate(' + d.x + ',' + d.y + ')'
+    })
   }
 
   function dragstarted(d) {
