320b74d0baa325a75346afe4ad39ff9d47fe5063
Dennis Dietrich <d.dietrich84@gmail.com>
2016-10-26 17:51:31 +0200

    escape message

--- blah-autoscale/src/logger.rs
+++ blah-autoscale/src/logger.rs
@@ -1,14 +1,16 @@
 use std::env;
 use log::{LogRecord, LogLevelFilter};
 use env_logger::LogBuilder;
-use chrono;
+use rustc_serialize::json::{encode};
+use chrono::Local;
 
 pub fn new_logger() {
     let format = |record: &LogRecord| {
+        let message = record.args().to_string();
         let string = vec![
-            format!("\"@timestamp\":\"{:?}\"", chrono::Local::now()),
+            format!("\"@timestamp\":\"{:?}\"", Local::now()),
             format!("\"@version\":\"{}\"", "1"),
-            format!("\"message\":\"{}\"", record.args()),
+            format!("\"message\":{}", encode(&message).unwrap()),
             format!("\"level\":\"{}\"", record.level()),
             format!("\"app\":\"{}\"", "autoscale"),
         ].join(",");
@@ -24,4 +26,11 @@ pub fn new_logger() {
     }
 
     builder.init().unwrap();
+}
+
+#[test]
+fn test_log() {
+    new_logger();
+    error!("{}", r#"{"foo": 42}"#);
+    error!("bar");
 }
