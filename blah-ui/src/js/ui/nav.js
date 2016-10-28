a4c4bb26ce1fe2aa28df2f9b5470125caef056dc
Dennis Dietrich <d.dietrich84@gmail.com>
2016-10-28 07:54:05 +0200

    nav

--- blah-ui/src/js/ui/nav.js
+++ blah-ui/src/js/ui/nav.js
@@ -1,15 +1,20 @@
 var h = require('snabbdom/h')
 var ctrl = require('../ctrl')
 
+function toggle(e) {
+  var elem = e.currentTarget
+  var display = elem.classList.contains('is-active') ? 'none' : 'block'
+  elem.classList.toggle('is-active')
+  var menus = elem.parentNode.querySelectorAll('.nav-menu');
+  [].forEach.call(menus, function(menu) {
+    menu.style.display = display
+  })
+}
+
 function render(model, update, ws, storage) {
   return h('nav.level.nav', [
     h('span.nav-toggle', {
-      on: {click: function(e) {
-        var elem = e.currentTarget
-        var display = elem.classList.contains('is-active') ? 'none' : 'block'
-        elem.classList.toggle('is-active')
-        elem.parentNode.querySelector('.nav-menu').style.display = display
-      }}
+      on: {click: toggle}
     }, [
       h('span'), h('span'), h('span')
     ]),
@@ -20,15 +25,24 @@ function render(model, update, ws, storage) {
       h('a.nav-item.button.is-link', {props: {href: '#/people'}}, 'People'),
       h('a.nav-item.button.is-link', {props: {href: '#/funnel'}}, 'Funnel'),
       h('a.nav-item.button.is-link', {props: {href: '#/segmentation'}}, 'Segmentation'),
-      h('a.nav-item.button.is-link', {props: {href: '#/world-map'}}, 'Map')
-    ]),
-    h('div.level-right.nav-menu', [
-      h('a.button.is-link', {props: {href: '#/jobs'}}, 'Jobs'),
-      h('a.button.is-link', {
-        on: {click: function(node) {
-          update(ctrl.theme, storage)
-        }}
-      }, [h('i.material-icons', 'color_lens')])
+      h('a.nav-item.button.is-link', {props: {href: '#/world-map'}}, 'Map'),
+      h('div.divider'),
+      h('div.dropdown.control', [
+        h('span.nav-toggle', {
+          on: {click: toggle}
+        }, [
+          h('span'), h('span'), h('span')
+        ]),
+        h('div.nav-menu', [
+          h('a.nav-item.button.is-link', {props: {href: '#/jobs'}}, 'Chronos Jobs'),
+          h('a.nav-item.button.is-link', {props: {href: 'http://kibana.blah.local'}}, 'Kibana'),
+          h('a.nav-item.button.is-link', {
+            on: {click: function(node) {
+              update(ctrl.theme, storage)
+            }}
+          }, [h('i.material-icons', 'color_lens')])
+        ])
+      ])
     ])
   ])
 }
