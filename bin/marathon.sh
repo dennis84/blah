49c2a33df767f2cdd60523b7a10d0f5dc64c66d0
Dennis Dietrich <d.dietrich84@gmail.com>
2017-04-19 07:27:41 +0200

    add local forwarding functionality

--- bin/marathon.sh
+++ bin/marathon.sh
@@ -6,6 +6,18 @@ create_app() {
     -d @- http://$BLAH_HOST:8080/v2/apps
 }
 
+create_forwarding_app() {
+  local port="$1"; shift
+  local paths="$@"
+  echo "Add haproxy entry for $BLAH_HOST:$port with path mapping: $paths"
+  cat "marathon/forwarding.json" | \
+    sed "s/\$BLAH_HOST/$BLAH_HOST/" | \
+    sed "s/\$PORT/$port/" | \
+    sed "s#\$PATHS#$paths#" | \
+    curl -s -o /dev/null -H "Content-Type: application/json" \
+    -d @- http://$BLAH_HOST:8080/v2/apps
+}
+
 destroy_app() {
   declare name="$1"
   echo "Destroy $name application"
